14c9a706acfd01646ed9772e2cd60ce0ff6ebe6a
egrapa <grapa42@yahoo.fr>
2018-07-14 22:17:09 +0200

    added keywords to be ignored during parsing of languages

--- preferences.php
+++ preferences.php
@@ -9,25 +9,25 @@ class Preferences
     // loaded from database
     public $BOOK_PAGES = 0;
     public $FILM_MINUTES = 0;
-    
+
     public $CONSUMER_KEY = "";
     public $CONSUMER_SECRET_KEY = "";
     public $OAUTH_TOKEN = "";
     public $OAUTH_SECRET_TOKEN = "";
-    
+
     // not loaded from database
-    public $KEYWORDS = array("#film", "#listen");
+    public $KEYWORDS = array("#film", "#listen", "#book", "#undo", "#read", "#audio", "#watch", "#listen", "#radio", "#movie");
     public $TARGET_BOOKS = 100;
     public $TARGET_FILMS = 100;
-    
+
     public $STAR_VALUE = 25;
     public $EPOCH = null;
 
     public $START_DATE = "";
     public $END_DATE = "";
 
-   
-    function __construct() 
+
+    function __construct()
     {
         $preferences = getPreferences();
         // $this->EPOCH = new DateTime("now");
@@ -40,35 +40,35 @@ class Preferences
         $this->FILM_MINUTES = $preferences['film_minutes'];
 
         $this->TARGETS = array(
-            'book' => $this->BOOK_PAGES, 
+            'book' => $this->BOOK_PAGES,
             'film' => $this->FILM_MINUTES);
-        
+
         $this->ACTIONS = array(
             'book' => 'inc_pagesread',
             'film' => 'inc_minuteswatched');
-        
+
         $this->ENTRYCONTENT = array(
             'book' => 'PagesRead',
             'film' => 'MinutesWatched');
-        
+
         $this->TAGS = array(
             'book' => array("#book","#read"),
             'film' => array("#film","#movie","#watch","#listen","#audio","#radio"));
-        
+
         $this->CHALLENGECONTENT = array(
             'book' => 'Books',
             'film' => 'Films');
-        
+
         $this->CONTENTTYPE = array(
             'pagesread' => 'book',
             'minuteswatched' => 'film');
-    
+
         $this->CONSUMER_KEY = $preferences['consumer_key'];
         $this->CONSUMER_SECRET_KEY = $preferences['consumer_secret_key'];
         $this->OAUTH_TOKEN = $preferences['oauth_token'];
         $this->OAUTH_SECRET_TOKEN = $preferences['oauth_secret_token'];
     }
-    
+
    function keywordExistsAtPosition($string, $position)
    {
         foreach($this->KEYWORDS as $keyword)
